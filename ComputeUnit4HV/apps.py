482791d738a702a5930235ede15313f4abafe446
Sam Chai <80421117@qq.com>
2023-07-30 12:30:42 +0800

    Initial Codes

--- ComputeUnit4HV/apps.py
+++ ComputeUnit4HV/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ComputeunitConfig(AppConfig):
+class Computeunit4HvConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ComputeUnit'
+    name = 'ComputeUnit4HV'
